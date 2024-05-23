4bcc9e5561b117c550e659242bf53f416e671fec
Jamie Bray <jamie@jamiebray.xyz>
2024-05-22 23:10:48 -0700

    70% improvement by breaking every loop into a separate function, allowing V8 to optimise better

--- src/nodesToJs.js
+++ src/nodesToJs.js
@@ -1,5 +1,8 @@
 const nodesToJs = (nodes, bitSize, memorySize) => {
   const memoryDataStructure = bitSize === 8 ? 'Uint8Array' : bitSize === 16 ? 'Uint16Array' : 'Uint32Array';
+  const maxValue = Math.pow(2, bitSize);
+
+  const startMethodName = nodesToJsRecursive(nodes, 0, 1, maxValue);
 
   return `// Auto-generated JS
 const m = new ${memoryDataStructure}(${memorySize});
@@ -9,34 +12,45 @@ function w(o) {
   write(String.fromCharCode(m[p + o]));
 }
 
-function go() {
-${nodesToJsRecursive(nodes, 0, 1)}
-};
-go();`;
+${methods}
+
+${startMethodName}()`;
 };
 
-const nodesToJsRecursive = (nodes, accumulatedOffset, indent) => {
-  const ii = '\t'.repeat(indent);
-  return nodes
+let methodNumber = 0;
+let methods = ``;
+
+const nodesToJsRecursive = (nodes, accumulatedOffset, indent, maxValue) => {
+  const code = nodes
     .flatMap(node => {
       const trueOffset = node.offset + accumulatedOffset;
       if (has(node.while)) {
-        const inner = nodesToJsRecursive(node.while.loop, trueOffset, indent + 1);
-        return `${ii}while (m[p + ${trueOffset}] ${node.while.not ? '!' : '='}== ${
-          node.while.value
-        }) {\n${inner}\n${ii}};`;
+        if (node.while.value <= -maxValue || node.while.value >= maxValue) throw new Error('bad!');
+        print(node.while.value, maxValue);
+        const methodName = nodesToJsRecursive(node.while.loop, trueOffset, indent + 1, maxValue);
+        return `\twhile (m[p + ${trueOffset}] ${node.while.not ? '!' : '='}== ${
+          node.while.value < 0 ? maxValue + node.while.value : node.while.value
+        }) {\n\t\t${methodName}()\n\t};`;
       } else if (has(node.add)) {
-        return `${ii}m[p + ${trueOffset}] += ${node.add};`;
+        return `\tm[p + ${trueOffset}] += ${node.add};`;
       } else if (has(node.move)) {
-        return `${ii}p += ${node.move};`;
+        return `\tp += ${node.move};`;
       } else if (has(node.input)) {
-        return `${ii}m[p + ${trueOffset}] = read();`;
+        return `\tm[p + ${trueOffset}] = read();`;
       } else if (has(node.output)) {
-        return `${ii}w(${trueOffset});`;
+        return `\tw(${trueOffset});`;
       } else if (has(node.set) && !node.nonTerminatingIfEven) {
-        return `${ii}m[p + ${trueOffset}] = ${node.set};`;
+        return `\tm[p + ${trueOffset}] = ${node.set};`;
       }
       throw new Error(`Un-handled node [${JSON.stringify(node)}]`);
     })
     .join('\n');
+
+  const methodName = `method${++methodNumber}`;
+
+  methods += `function ${methodName}(){
+${code}
+}\n`;
+
+  return methodName;
 };
